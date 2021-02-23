090802f68e4a0c3bdd47eef6fda9e8e1136c6cab
Andres Lopez Feijoo <andres_lf@icloud.com>
2021-02-22 22:54:48 -0300

    mas funciones

--- music/BuscaEscalas.py
+++ music/BuscaEscalas.py
@@ -1,7 +1,7 @@
 Mayor = [0, 2, 4, 5, 7, 9, 11]
-Menor = [0, 2, 3, 5, 7, 8, 11]
+Menor = [0, 2, 3, 5, 7, 8, 10]
 notas = ["Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"]
-
+notas2 = ["Dob", "Do", "Reb", "Re", "Mib", "Mi", "Fa", "Solb", "Sol", "Lab", "La", "Sib"]
 notain = input("Indica una nota de partida: ")
 tipoin = input("Indicá el tipo de escala: ")
 
@@ -10,36 +10,69 @@ if tipoin == "Mayor": #Bien acá apuntando tipo a la lista patrón
 elif tipoin == "Menor":
     tipo = Menor
 
+if notain == "Fa" or "Sib" or "Mib" or "Lab" or "Reb" or "Solb" or "Dob" or "Fab":
+    notas = notas2
+
+
 def nota_a_numero(nota): #Este está perfecto
     nota_nro = 0
     if nota=="Do":
         nota_nro = 0
+    elif nota=="Dob":
+        nota_nro = 0
     elif nota=="Sol":
+        nota_nro = 7
+    elif nota=="Solb":
         nota_nro = 7
     elif nota == "Re":
         nota_nro = 2
+    elif nota=="Reb":
+        nota_nro = 2
     elif nota=="La":
+        nota_nro = 9
+    elif nota=="Lab":
         nota_nro = 9
     elif nota=="Mi":
         nota_nro = 4
+    elif nota=="Mib":
+        nota_nro = 4
     elif nota=="Si":
+        nota_nro = 11
+    elif nota=="Sib":
         nota_nro = 11
     elif nota=="Fa#":
         nota_nro = 6
-        notas[5] = "Mi#"
+    elif nota == "Fa":
+        nota_nro = 6
     elif nota=="Do#":
         nota_nro = 1
         notas[0] = "Si#"
+        notas[5] = "Mi#"
 
     return nota_nro
 
-def quiero_una_escala(nro_partida, patron):
 
+def nuevo_patron(numero):
+    NvoPatr = []
+    for i in tipo:
+        i = ((i + numero) % 12)
+        NvoPatr.append(i)
+    return NvoPatr
+
+def devuelvo_una_escala(patron):
+    escala = []
     for i in patron:
-        escala = ((i + nro_partida) % 12)
-        print(notas[escala])
+       armoescala = notas[i]
+       escala.append(armoescala)
+    return escala
 
-#print(quiero_una_escala(nota_a_numero(notain), tipo))
-quiero_una_escala(nota_a_numero(notain), tipo)
-#Acá estabas imprimiendo un None al final porque print no sabía que imprimir porque la función
-#quiero_una_escala no devuelve nada (tiene print adentro)
+print(devuelvo_una_escala(nuevo_patron(nota_a_numero(notain))))
+
+
+
+
+
+
+
+
+
